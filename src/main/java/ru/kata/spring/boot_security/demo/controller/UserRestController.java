7c26610b9737f0ffbac131db5dcd28b776618166
Kirill <kornienko.krll@gmail.com>
2026-07-31 16:04:54 +0400

    update

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
@@ -1,6 +1,8 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.Role;
@@ -21,42 +23,52 @@ public class UserRestController {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public List<User> getAllUsers() {
-        return userService.getAllUser();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUser());
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public User getUser(@PathVariable Long id) {
-        return userService.getUserByIdWithRoles(id);
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getUserByIdWithRoles(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("/me")
-    public User getCurrentUser() {
-        return userService.getCurrentUserFromContext();
+    public ResponseEntity<User> getCurrentUser() {
+        User user = userService.getCurrentUserFromContext();
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("/roles")
-    public List<Role> getRoles() {
-        return roleService.getAllRoles();
+    public ResponseEntity<List<Role>> getRoles() {
+        return ResponseEntity.ok(roleService.getAllRoles());
     }
 
     @PostMapping()
     @PreAuthorize("hasRole('ADMIN')")
-    public void AddUser(@RequestBody User user) {
+    public ResponseEntity<Void> AddUser(@RequestBody User user) {
         userService.saveUserWithRolesAndPassword(user, user.getRoleIds());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public void editUser(@RequestBody User user, @PathVariable Long id) {
-        System.out.println("Получен пользователь: " + user);
+    public ResponseEntity<Void> editUser(@RequestBody User user, @PathVariable Long id) {
         userService.updateUser(user, id);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
